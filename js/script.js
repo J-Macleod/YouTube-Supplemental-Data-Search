df01b4620b8ba72dc7361d95099f704e8bd9e0f7
J-Macleod <jacob.macleodnin@gmail.com>
2022-09-30 02:53:57 -0400

    Channel Name and ID API Functionality

--- js/script.js
+++ js/script.js
@@ -1,29 +1,61 @@
 const yt = "AIzaSyCQHhOngXMnSmz4bZbwhnH29qs-9PSq0-M"
 
+idURLBase = "https://www.youtube.com/channel/"
+nameURLBase = "https://www.youtube.com/c/"
+
 function getChannelID(userURL) {
-    id  = userURL.replace("https://www.youtube.com/channel/", "")
+    id  = userURL.replace(idURLBase, "")
     return id
 }
 
-function getUserImg(userURL) {
-    let ajaxImg = new XMLHttpRequest()
-    ajaxImg.onreadystatechange = function() {
-        if (ajaxImg.readyState == 4) {
+function getChannelName(userURL) {
+    channelName = userURL.replace(nameURLBase, "")
+    return channelName
+}
 
-            if ( (ajaxImg.status >= 200 && ajaxImg.status < 300) || (ajaxImg.status == 304) ) {
-                let data = JSON.parse(ajaxImg.responseText)
+async function getUserImg(userURL) {
+    if (userURL.includes(idURLBase)) {
+        let ajaxImg = new XMLHttpRequest()
+        ajaxImg.onreadystatechange = function() {
+            if (ajaxImg.readyState == 4) {
 
-                let profImgHTML = "<p><img src='"+data.items[0]["snippet"]["thumbnails"]["default"]["url"]+"' alt='User Profile Picture' width='150' height='150'></p>"
-                                
-                document.getElementById('results').innerHTML = profImgHTML
+                if ( (ajaxImg.status >= 200 && ajaxImg.status < 300) || (ajaxImg.status == 304) ) {
+                    let data = JSON.parse(ajaxImg.responseText)
+
+                    let profImgHTML = "<p><img src='"+data.items[0]["snippet"]["thumbnails"]["default"]["url"]+"' alt='User Profile Picture' width='150' height='150'></p>"
+                                    
+                    document.getElementById('results').innerHTML = profImgHTML
+                }
             }
         }
+
+        let channelID = getChannelID(userURL)
+        let imgURL = "https://www.googleapis.com/youtube/v3/channels?part=snippet&fields=items%2Fsnippet%2Fthumbnails%2Fdefault&id="+channelID+"&key="+yt
+        
+        ajaxImg.open('GET', imgURL, true)
+        ajaxImg.send()
+    } else if (userURL.includes(nameURLBase)) {
+        let ajaxImg = new XMLHttpRequest()
+        ajaxImg.onreadystatechange = function() {
+            if (ajaxImg.readyState == 4) {
+
+                if ( (ajaxImg.status >= 200 && ajaxImg.status < 300) || (ajaxImg.status == 304) ) {
+                    let data = JSON.parse(ajaxImg.responseText)
+
+                    let profImgHTML = "<p><img src='"+data.items[0]["snippet"]["thumbnails"]["default"]["url"]+"' alt='User Profile Picture' width='150' height='150'></p>"
+                                    
+                    document.getElementById('results').innerHTML = profImgHTML
+                }
+            }
+        }
+
+        let channelID = getChannelName(userURL)
+
+        let imgURL = "https://www.googleapis.com/youtube/v3/channels?part=snippet&fields=items%2Fsnippet%2Fthumbnails%2Fdefault&forUsername="+channelID+"&key="+yt
+        
+        ajaxImg.open('GET', imgURL, true)
+        ajaxImg.send()
     }
-    
-    let channelID = getChannelID(userURL)
-    let imgURL = "https://www.googleapis.com/youtube/v3/channels?part=snippet&fields=items%2Fsnippet%2Fthumbnails%2Fdefault&id="+channelID+"&key="+yt
-    ajaxImg.open('GET', imgURL, true)
-    ajaxImg.send()
 }
 
 document.getElementById("enterButton").onclick = function() {    
